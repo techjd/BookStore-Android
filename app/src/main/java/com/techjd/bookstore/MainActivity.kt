c804402e9cddf330e4f054d54817179a6ec6ad15
techjd <jaydeepparmar253@gmail.com>
2022-06-26 00:14:07 +0530

    Login UI , Registration UI, and Added Dependencies

---
+++ app/src/main/java/com/techjd/bookstore/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.techjd.bookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
