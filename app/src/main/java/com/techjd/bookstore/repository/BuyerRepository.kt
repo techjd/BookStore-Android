80a6f52362eb885b830d77841faa987a33b46423
techjd <jaydeepparmar253@gmail.com>
2022-06-28 18:44:24 +0530

    Added New functions in buyer

--- app/src/main/java/com/techjd/bookstore/repository/BuyerRepository.kt
+++ app/src/main/java/com/techjd/bookstore/repository/BuyerRepository.kt
@@ -1,10 +1,34 @@
 package com.techjd.bookstore.repository
 
+import androidx.lifecycle.LiveData
+import androidx.room.PrimaryKey
 import com.techjd.bookstore.api.BuyerAPI
 import com.techjd.bookstore.api.UserAPI
+import com.techjd.bookstore.db.dao.CartDao
+import com.techjd.bookstore.db.models.Data
+import com.techjd.bookstore.models.books.Books
 import javax.inject.Inject
 
-class BuyerRepository @Inject constructor(private val buyerAPI: BuyerAPI) {
+class BuyerRepository @Inject constructor(
+    private val buyerAPI: BuyerAPI,
+    private val cartDao: CartDao
+) {
 
     suspend fun getAllBooks() = buyerAPI.getAllBooks()
+
+    suspend fun insertBooks(book: Data) = cartDao.insertBook(book)
+
+    suspend fun updateBook(book: Data) = cartDao.updateBook(book)
+
+    suspend fun deleteBook(book: Data) = cartDao.deleteBook(book)
+
+    suspend fun getAddress(token: String) = buyerAPI.getAddress(token)
+
+    val getAllBooksOfCard = cartDao.getAllBooks()
+
+    suspend fun checkIfBookExists(id: String) = cartDao.checkIfItemExists(id)
+
+    val getTotal: LiveData<Int> = cartDao.getTotal()
+
+    val getTotalCountInCart: LiveData<Int> = cartDao.getCountInCart()
 }
--- app/src/main/java/com/techjd/bookstore/viewmodels/BuyerViewModel.kt
+++ app/src/main/java/com/techjd/bookstore/viewmodels/BuyerViewModel.kt
@@ -1,9 +1,12 @@
 package com.techjd.bookstore.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.techjd.bookstore.db.models.Data
+import com.techjd.bookstore.models.address.Address
 import com.techjd.bookstore.models.books.Books
 import com.techjd.bookstore.repository.BuyerRepository
 import com.techjd.bookstore.utils.ErrorResponse
@@ -22,6 +25,18 @@ class BuyerViewModel @Inject constructor(
     private val _allBooks: MutableLiveData<NetworkResult<Books>> = MutableLiveData()
     val allBooks: LiveData<NetworkResult<Books>> = _allBooks
 
+    val allBooksFromCart: LiveData<List<Data>> = buyerRepository.getAllBooksOfCard
+
+    private val _doesItemExist: MutableLiveData<Boolean> = MutableLiveData()
+    val doesItemExist: LiveData<Boolean> = _doesItemExist
+
+    val totalAmount: LiveData<Int> = buyerRepository.getTotal
+
+    val totalItemsInCart: LiveData<Int> = buyerRepository.getTotalCountInCart
+
+    private val _address: MutableLiveData<NetworkResult<Address>> = MutableLiveData()
+    val address: LiveData<NetworkResult<Address>> = _address
+
     fun getAllBooks() {
         viewModelScope.launch {
             _allBooks.postValue(NetworkResult.Loading())
@@ -32,6 +47,59 @@ class BuyerViewModel @Inject constructor(
                 val error = errorResponse.giveErrorResult(data.errorBody()!!)
                 _allBooks.postValue(NetworkResult.Error(error.message))
             }
+        }
+    }
+
+    fun getAddress(token: String) {
+        viewModelScope.launch {
+            _address.postValue(NetworkResult.Loading())
+            val data = buyerRepository.getAddress(token)
+            if (data.isSuccessful) {
+                _address.postValue(NetworkResult.Success(data.body()!!))
+            } else {
+                val error = errorResponse.giveErrorResult(data.errorBody()!!)
+                _address.postValue(NetworkResult.Error(error.message))
+            }
+        }
+    }
+
+//    fun getAllBooksFromCart() {
+//        viewModelScope.launch {
+//            val data = buyerRepository.getAllBooksOfCard()
+//            _allBooksFromCart.postValue(data.value)
+//        }
+//    }
+
+//    fun getTotalPrice() {
+//        viewModelScope.launch {
+//            val data = buyerRepository.getTotal()
+//            Log.d("TOTAL VALUE ", "getTotalPrice: ${data.value}")
+//            _totalAmount.postValue(data.value)
+//        }
+//    }
+
+    fun updateBook(book: Data) {
+        viewModelScope.launch {
+            buyerRepository.updateBook(book)
+        }
+    }
+
+    fun deleteBook(book: Data) {
+        viewModelScope.launch {
+            buyerRepository.deleteBook(book)
+        }
+    }
+
+    fun checkIfItemExists(id: String) {
+        viewModelScope.launch {
+            val data = buyerRepository.checkIfBookExists(id)
+            _doesItemExist.postValue(data)
+        }
+    }
+
+    fun insertBook(book: Data) {
+        viewModelScope.launch {
+            buyerRepository.insertBooks(book)
         }
     }
 
